f010d6087cef6bc0eb6ce199a19e1e82ea2dfb73
1049906948 <1049906948@qq.com>
2019-11-09 22:40:22 +0800

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="about">
+    <div class="header">名校名师网</div>
+
+    <div class="nav">导航</div>
+    <div class="main">
+      <div class="mod"></div>
+      <div class="mod2"></div>
+      <div class="mod2"></div>
+      <div class="mod2"></div>
+      <div class="mod2"></div>
+    </div>
+    <div class="btn">
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+    </div>
+
+    <div class="footer"></div>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  list-style: none;
+}
+.header {
+  width: 100%;
+  height: 50px;
+  background: red;
+}
+
+.nav {
+  width: 70%;
+  height: 300px;
+  background: blanchedalmond;
+  margin: auto;
+}
+
+.main {
+  width: 70%;
+  /* height: 1000px; */
+  border: 1px solid;
+  margin: auto;
+}
+.main .mod {
+  border: 1px solid;
+  height: 50px;
+}
+.main .mod2 {
+  border: 1px solid;
+  height: 200px;
+}
+
+.footer {
+  background: #ccc;
+  width: 100%;
+  height: 100px;
+}
+.btn {
+  width: 200px;
+  /* height: 1000px; */
+  /* border: 1px solid; */
+  margin: auto;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+     <h1>首页</h1>
+  </div>
+</template>
+
+
+
+<style  scoped>
+
+
+</style>
+
+
